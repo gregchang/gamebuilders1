6f222f4a84125342dc2f69130ad1d6c011830d7d
Mawlerest <luks.mawl@gmail.com>
2015-09-05 11:18:02 -0500

    Movement

---
+++ DeezNuts/Assets/Scripts/Move.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+	public GameObject Player;
+	Rigidbody2D PlayerR;
+
+	// Use this for initialization
+	void Start () {
+		PlayerR=Player.GetComponent<Rigidbody2D> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (PlayerR.velocity.y<10 && PlayerR.velocity.y>-10) {
+			PlayerR.AddForce(new Vector2(0,Input.GetAxis ("Vertical")*10));
+		}
+		if (PlayerR.velocity.x<10 && PlayerR.velocity.x>-10) {
+			PlayerR.AddForce(new Vector2(Input.GetAxis ("Horizontal")*10,0));
+		}
+	
+	}
+}
