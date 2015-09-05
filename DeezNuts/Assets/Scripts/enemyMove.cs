229e570912efc20200ada68ca6712744132809f0
Mawlerest <luks.mawl@gmail.com>
2015-09-05 17:54:12 -0500

    Level1 and Main mechanics

--- DeezNuts/Assets/Scripts/enemyMove.cs
+++ DeezNuts/Assets/Scripts/enemyMove.cs
@@ -3,24 +3,22 @@ using System.Collections;
 
 public class enemyMove : MonoBehaviour {
 	public GameObject Player;
-	Rigidbody2D PlayerR;
 	public GameObject Enemy;
 	Rigidbody2D EnemyR;
+	public int npoints=2;
 	public static int numPoints=2;
 	public int lives=3;
 	public int speed=5;
+	public int distance=3;
 	int next=0;
 
-	public GameObject[] w= new GameObject[numPoints];
-
-	bool switch1 = false;
+	public GameObject[] w;
+	
 	bool follow = false;
 	
 	// Use this for initialization
 	void Start () {
-		PlayerR=Player.GetComponent<Rigidbody2D> ();
 		EnemyR=Enemy.GetComponent<Rigidbody2D> ();
-
 		
 	}
 
@@ -38,9 +36,8 @@ public class enemyMove : MonoBehaviour {
 	void Update () {
 		// Move Enemy towards Pkayer
 
-
 		float step = 3.0f * Time.deltaTime;
-		if ((Vector2.Distance (Player.transform.position, Enemy.transform.position)) < 5) {
+		if ((Vector2.Distance (Player.transform.position, Enemy.transform.position)) < distance) {
 
 			follow = true;
 
@@ -58,12 +55,11 @@ public class enemyMove : MonoBehaviour {
 			//Debug.Log (transform.position + " " + w1.transform.position);
 
 			if (transform.position.ToString ().Equals (w [next].transform.position.ToString ())) {
-				switch1 = true;
 				next++;
-				if(next>=numPoints){
+				if(next>=w.Length){
 					next=0;
 				}
-				Debug.Log ("works");
+				//Debug.Log ("works");
 			}
 		}
 	}
