2b617cf23505cc9347f64b67148e0687ce2798c6
benson-w <macuser3@wirelessprvnat-172-17-100-156.near.illinois.edu>
2015-09-05 21:28:19 -0500

    titlepage

---
+++ DeezNuts/Assets/Scripts/Titlepage.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Titlepage : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		//Debug.Log (Input.GetAxis ("Fire1"));
+
+		if(Input.GetAxis("Fire1")>0) {
+			Application.LoadLevel("Level1");
+	}
+
+
+}
+}
